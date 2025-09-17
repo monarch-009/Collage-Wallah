4f8a3050bafc7e5f09fff28a2ae6af7ef3bccbc7
Aditya <adityasinghmoni@gmail.com>
2025-09-17 11:09:06 +0530

    input output

---
+++ Lecture-1/input_output.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // Output in C++
+    cout << "Hello World" << "\n";
+    cout << "Aditya Singh" << endl;
+
+    // Input in C++
+    string name;
+    int age;
+
+    cin >> name >> age;
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+
+    return 0;
+}
